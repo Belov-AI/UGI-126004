51694716e33f6b2ad678ac9fb561339bcff0d27c
Belov-AI <Belov.Alexander@urfu.ru>
2022-09-30 16:04:35 +0500

    Проект Arithmetics

---
+++ Arithmetics/Arithmetics/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmetics
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+
+            Console.WriteLine("Введите значение а");
+            a = int.Parse(Console.ReadLine());
+
+            int x = a++ + ++a;
+
+            Console.WriteLine("x = " + x);
+            Console.WriteLine("a = " + a);
+
+            int y;
+            x = y = 0;
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+
+            Console.ReadKey();
+        }
+    }
+}
