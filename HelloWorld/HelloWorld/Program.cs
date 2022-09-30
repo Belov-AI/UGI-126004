c3b692cd7b1c038ef8f0b0cdf3acd1944c6789e1
Belov-AI <Belov.Alexander@urfu.ru>
2022-09-30 14:50:52 +0500

    Добавлена программа HelloWorld

---
+++ HelloWorld/HelloWorld/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Привет, мир!");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.BackgroundColor = ConsoleColor.White;
+            Console.WriteLine(1 + 3);
+
+            Console.ReadKey();
+        }
+    }
+}
