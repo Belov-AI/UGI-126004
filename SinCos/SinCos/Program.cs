edd1d37639ce64130644509b2e666cee5d7cd5df
Belov-AI <Belov.Alexander@urfu.ru>
2022-10-14 15:22:44 +0500

    Выделение метода

--- SinCos/SinCos/Program.cs
+++ SinCos/SinCos/Program.cs
@@ -6,19 +6,18 @@ namespace SinCos
     {
         static void Main()
         {
-            double angle = 15 * Math.PI / 180;
-            Console.WriteLine("sin(15°) = " + Math.Round(Math.Sin(angle), 3));
-            Console.WriteLine("cos(15°) = " + Math.Round(Math.Cos(angle), 3));
-
-            angle = 37 * Math.PI / 180;
-            Console.WriteLine("sin(37°) = " + Math.Round(Math.Sin(angle), 3));
-            Console.WriteLine("cos(37°) = " + Math.Round(Math.Cos(angle), 3));
-
-            angle = 113 * Math.PI / 180;
-            Console.WriteLine("sin(113°) = " + Math.Round(Math.Sin(angle), 3));
-            Console.WriteLine("cos(113°) = " + Math.Round(Math.Cos(angle), 3));
-
+            PrintSinusCosinus(15);
+            PrintSinusCosinus(37);
+            PrintSinusCosinus(113);
+            
             Console.ReadKey();
+        }
+
+        static void PrintSinusCosinus(double angleInDegrees)
+        {
+            var angleInRadians = angleInDegrees * Math.PI / 180;
+            Console.WriteLine("sin(" + angleInDegrees + "°) = " + Math.Round(Math.Sin(angleInRadians), 3));
+            Console.WriteLine("cos(" + angleInDegrees + "°) = " + Math.Round(Math.Cos(angleInRadians), 3));
         }
     }
 }
