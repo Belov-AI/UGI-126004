ca01b31a2d36d486ba664f837147c095ce576b71
Belov-AI <Belov.Alexander@urfu.ru>
2022-11-07 13:57:25 +0500

    Логический тип данных

---
+++ IntLogical/IntLogical/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace IntLogical
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            var x = Input("x");
+            var y = Input("y");
+
+            PrintResult(~x, "~x");
+            PrintResult(x & y, "x & y");
+            PrintResult(x | y, "x | y");
+            PrintResult(x ^ y, "x ^ y");
+
+            Console.ReadKey();
+        }
+
+        private static void PrintResult(int number, string name)
+        {
+            Console.WriteLine($"{name} = {Convert.ToString(number, 2)}");
+        }
+
+        static int Input(string name)
+        {
+            Console.WriteLine($"Введите {name}:");
+            var number = Convert.ToInt32(Console.ReadLine(),16);
+            Console.WriteLine($"{name} = {Convert.ToString(number, 2)}\n");
+            return number;
+        }
+    }
+}
---
+++ Logic/Logic/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Logic
+{
+    class Program
+    {
+        static void Main()
+        {
+            var x = Input("x");
+            var y = Input("y");
+
+            Console.WriteLine($"!x = {!x}, !y = {!y}\nx | y = {x || y}, x & y = {x && y}, x ^ y = {x ^ y}");
+
+            Console.ReadKey();
+        }
+
+        private static bool Input(string name)
+        {
+            Console.WriteLine($"Введите {name}:");
+            var logicalValue = bool.Parse(Console.ReadLine());
+            Console.WriteLine(logicalValue);
+            return logicalValue;
+        }
+    }
+}
