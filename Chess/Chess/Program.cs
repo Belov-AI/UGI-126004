92900b8ed5094a816e2c139faa47bd018fdde547
Belov-AI <Belov.Alexander@urfu.ru>
2022-11-14 13:56:28 +0500

    IfElse

---
+++ Chess/Chess/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Chess
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Введите позицию белой пешки");
+            string whitePownPosition = Console.ReadLine();
+
+            if(!CheckWhitePownPosition(whitePownPosition))
+            {
+                Console.WriteLine("Позиция белой пешки не корректина");
+                Console.ReadKey();
+                return;
+            }
+
+            
+            Console.ReadKey();
+        }
+
+        static bool CheckWhitePownPosition(string position)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
