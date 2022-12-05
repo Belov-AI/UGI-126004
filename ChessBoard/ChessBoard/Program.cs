b372ecd15ba0f3770a940d9d91d0736769c736d9
Belov-AI <Belov.Alexander@urfu.ru>
2022-12-05 13:53:34 +0500

    Циклы for, while и do-while

--- ChessBoard/ChessBoard/Program.cs
+++ ChessBoard/ChessBoard/Program.cs
@@ -5,17 +5,44 @@ namespace Chessboard
 {
     class Program
     {
+        static ConsoleColor lightColor = ConsoleColor.Yellow;
+        static ConsoleColor darkColor = ConsoleColor.Red;
         static void Main()
         {
-            int boardSize;
+            int boardSize = 0;
 
             //Здесь ввод размера доски.
             //оформите его в виде бесконечного цикла с проверкой,
             //что 1<= boardSize <= 26
             //и предложением повторного ввода, если условие не выполняется
             //Предусмотрите отказ от ввода (с последующим завершением программы).
-            
-            PrintBoard(boardSize);           
+
+            Console.WriteLine("Введите размер доски от 1 до 26. Enter - отказ от ввода");
+
+            string input;
+
+            while (true)
+            {
+                input = Console.ReadLine();
+
+                if (input == "")
+                    break;
+
+                boardSize = int.Parse(input);
+
+                if( boardSize < 1 || boardSize > 26)
+                {
+                    Console.WriteLine("Введите размер доски от 1 до 26.");
+                    continue;
+                }
+
+                break;
+            }
+
+            if (input == "")
+                return;
+
+            PrintBoard(boardSize);
 
             Console.ReadKey();
         }
@@ -23,6 +50,45 @@ namespace Chessboard
         static void PrintBoard(int size)
         {
             //написать метод печати шахматной доски размера (size x size)
+            PrintHeader(size);
+
+            for (var row = size; row > 0; row--)
+            {
+                Console.Write($"{row,2}");
+
+                ConsoleColor firstColor, secondColor;
+
+                if (row % 2 == 0)
+                {
+                    firstColor = lightColor;
+                    secondColor = darkColor;
+                }
+                else
+                {
+                    firstColor = darkColor;
+                    secondColor = lightColor;
+                }
+
+                for (var i = 0; i < size; i++)
+                    if (i % 2 == 0)
+                        PrintSquare(firstColor);
+                    else
+                        PrintSquare(secondColor);
+
+                Console.WriteLine(row);
+            }
+
+            PrintHeader(size);
+        }
+
+        static void PrintHeader(int size)
+        {
+            Console.Write("  ");
+
+            for(var i = 0; i < size; i++)
+                Console.Write((char)(0x61 + i));
+
+            Console.WriteLine();
         }
 
         /// <summary>
---
+++ Factorials/Factorials/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Factorials
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            long f = 1;
+            int n = 1;
+
+            do
+            {
+                f *= n;                
+                Console.WriteLine($"{n}! = {f}");
+                n++;
+            } while (f < long.MaxValue / n);
+
+            Console.WriteLine("Вычисление закончено.");
+
+            Console.ReadKey();
+        }
+    }
+}
