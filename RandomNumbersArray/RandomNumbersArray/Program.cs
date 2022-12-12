44ff7cab4b8992ee2711e9c47b733bc14f87c5de
Belov-AI <Belov.Alexander@urfu.ru>
2022-12-12 13:56:26 +0500

    Массивы

---
+++ RandomNumbersArray/RandomNumbersArray/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RandomNumbersArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = new int[10];
+            PrintIntArray(numbers);
+
+            var rnd = new Random();
+
+            for (int i = 0; i < numbers.Length; i++)
+                numbers[i] = rnd.Next(1000);
+
+            PrintIntArray(numbers);
+
+            Array.Sort(numbers);
+            PrintIntArray(numbers);
+
+            Console.ReadKey();
+        }
+
+        static void PrintIntArray(int[] array)
+        {
+            foreach( var number in array)
+                Console.WriteLine(number);
+
+            Console.WriteLine();
+        }
+    }
+}
