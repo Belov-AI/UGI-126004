8046e4832f07ec33f83da2b8a8362d0e601ee90a
Belov-AI <Belov.Alexander@urfu.ru>
2023-04-24 16:25:54 +0500

    Словари и стеки

---
+++ Brackets/Brackets/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var brackets = new Dictionary<char, char>();
+            brackets[')'] = '(';
+            brackets[']'] = '[';
+            brackets['}'] = '{';
+
+            Console.WriteLine("Введите выражение со скобками");
+            var expression = Console.ReadLine();
+
+            if (IsCorrect(expression, brackets))
+                Console.WriteLine("Скобки расставлены верно");
+            else
+                Console.WriteLine("Скобки расставлены неверно");
+
+            Console.ReadKey();
+        }
+
+        static bool IsCorrect(string expression, Dictionary<char,char> brackets)
+        {
+            var stack = new Stack<char>();
+
+            for (var i = 0; i < expression.Length; i++)
+            {
+                var c = expression[i];
+
+                if (brackets.ContainsValue(c))
+                    stack.Push(c);
+                else if (brackets.ContainsKey(c))
+                    if (stack.Count == 0 || stack.Pop() != brackets[c])
+                        return false;
+            }
+
+            return stack.Count == 0;
+        }
+    }
+}
---
+++ Frequencies/Frequencies/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Frequencies
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var words = new[] { "a", "abc", "ab", "a", "aba", "ab", "a", "b" };
+
+            var dict = new Dictionary<string, int>();
+
+            foreach (var word in words)
+            {
+                if(dict.ContainsKey(word))
+                    dict[word]++;
+                else
+                    dict[word] = 1;
+            }
+
+            PrintDictionary(dict);
+
+            Console.WriteLine();
+
+            var sortedDict = new SortedDictionary<string, int>(dict);
+            
+            PrintDictionary(sortedDict);
+
+            Console.ReadKey();
+        }
+
+        static void PrintDictionary(IDictionary<string, int> dic)
+        {
+            //foreach (var elem in dic)
+            //    Console.WriteLine($"{elem.Key}\t{elem.Value}");
+            foreach (var key in dic.Keys)
+                Console.WriteLine($"{key}\t{dic[key]}");
+        }        
+    }
+}
