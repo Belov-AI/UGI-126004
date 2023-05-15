5199394fd438dd457fbd2d498fb332a131bd3929
Belov-AI <Belov.Alexander@urfu.ru>
2023-05-15 16:03:28 +0500

    Очереди. Проект MazeLibrary. Начало

---
+++ MazeLibrary/MazeLibrary/Maze.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MazeLibrary
+{
+    public class Maze
+    {
+        Cell[,] board;
+
+        public int Height => board.GetLength(0);
+        public int Width => board.GetLength(1);
+    }
+}
---
+++ Queues/Queues/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Queues
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите количество знаков числа");
+            var k = int.Parse(Console.ReadLine());
+            int min = (int)Math.Pow(10, k - 1);
+            int max = (int)Math.Pow(10, k); 
+
+            Console.WriteLine("Введиьте количество чисел");
+            var n = int.Parse(Console.ReadLine());
+
+            var numbers = new SortedDictionary<int, Queue<int>>();
+
+            var random = new Random();
+
+            for(var i = 0; i < n;  i++)
+            {
+                var number = random.Next(min, max);
+                var key = number % 10;
+
+                if(!numbers.ContainsKey(key))
+                    numbers[key] = new Queue<int>();
+                 
+                numbers[key].Enqueue(number);
+            }
+
+            foreach(var key in numbers.Keys)
+                Print(numbers[key]);
+
+            Console.ReadKey();
+        }
+
+        static void Print(IEnumerable<int> nums)
+        {
+            Console.WriteLine();
+
+            foreach (var num in nums)
+                Console.Write($"{num} ");
+
+            Console.WriteLine();
+        }
+    }
+}
